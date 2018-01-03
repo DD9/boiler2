c54b8c8cf7254e486b00e000963384b3f7704aa1
Hilary Clarcq <hilary@dd9.com>
2018-01-03 11:50:25 -0700

    add current/active classes to CPT menu items

--- functions.php
+++ functions.php
@@ -106,6 +106,26 @@ This is a good idea in many cases. Otherwise use:  https://wordpress.org/plugins
 // } 
 // add_action( 'after_switch_theme', 'boiler_flush_rewrite_rules' );
 
+/* 
+Add active/current class to CPT menu items
+https://gist.github.com/gerbenvandijk/5253921
+*/
+
+//function custom_active_item_classes($classes = array(), $menu_item = false) {
+    //global $post;
+
+    //// Get post ID, if nothing found set to NULL
+    //$id = ( isset( $post->ID ) ? get_the_ID() : NULL );
+
+    //// Checking if post ID exist...
+    //if (isset( $id )){
+	    //$classes[] = ($menu_item->url == get_post_type_archive_link($post->post_type)) ? 'current-menu-item active' : '';
+    //}
+
+    //return $classes;
+//}
+//add_filter( 'nav_menu_css_class', 'custom_active_item_classes', 10, 2 );
+
 
 
 
