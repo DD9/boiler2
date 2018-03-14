bbdca3e5b470ec1ab7657184923a4780a539c98e
Conner Simmering <connersimmering@gmail.com>
2018-03-14 13:15:06 -0600

    Activated Gulp JS compilation & optimization, bones.php now expects a custom.min.js file created from scripts.js. Added watch functionality for scripts.js. VendorJS working as expected.

--- gulpfile.js
+++ gulpfile.js
@@ -41,8 +41,8 @@ var jsVendorFile            = 'vendors'; // Compiled JS vendors file name.
 // Default set to vendors i.e. vendors.js.
 
 // JS Custom related.
-var jsCustomSRC             = './assets/js/custom/*.js'; // Path to JS custom scripts folder.
-var jsCustomDestination     = './assets/js/'; // Path to place the compiled JS custom scripts file.
+var jsCustomSRC             = './js/scripts.js'; // Path to JS custom scripts folder.
+var jsCustomDestination     = './js/'; // Path to place the compiled JS custom scripts file.
 var jsCustomFile            = 'custom'; // Compiled JS custom file name.
 // Default set to custom i.e. custom.js.
 
