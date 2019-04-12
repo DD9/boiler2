62a3798878aa095245f42861961130a466871edb
Todd Dumas <todd@dd9.com>
2019-04-11 22:02:44 -0600

    Clean up legacy crud from header

--- header.php
+++ header.php
@@ -1,16 +1,11 @@
 <!doctype html>
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<html <?php language_attributes(); ?> class="">
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-<title><?php if (is_front_page()) { bloginfo('name'); } else { wp_title(''); } ?></title>
+<title><?php wp_title(''); ?></title>
 
-<?php //<meta name="HandheldFriendly" content="True">  ?>
-<?php //<meta name="MobileOptimized" content="320">  ?>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
